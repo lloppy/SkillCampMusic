28b97a4d69a2cdad10e2970192b1f0d81a3a995d
Полина <ankudinovapol@gmail.com>
2023-01-14 02:14:54 +0500

    create GridRecyclerAdapter

---
+++ app/src/main/java/com/example/skillcampmusic/ui/grid_recycler/GridRecyclerAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.skillcampmusic.ui.grid_recycler
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class GridRecyclerAdapter: RecyclerView.Adapter<GridRecyclerAdapter.GridViewHolder>() {
+    class GridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GridViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: GridViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/example/skillcampmusic/ui/grid_recycler/GridRecyclerFragment.kt
+++ app/src/main/java/com/example/skillcampmusic/ui/grid_recycler/GridRecyclerFragment.kt
@@ -1,8 +1,15 @@
 package com.example.skillcampmusic.ui.grid_recycler
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.skillcampmusic.R
 
 class GridRecyclerFragment : Fragment(R.layout.fragment_grid_recycler) {
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
