329e4a33d03bb2bce490589cbc05ff4766355061
Полина <ankudinovapol@gmail.com>
2023-01-14 02:34:38 +0500

    Adapter done

---
+++ app/src/main/java/com/example/skillcampmusic/ui/grid_recycler/GridItem.kt
@@ -0,0 +1,8 @@
+package com.example.skillcampmusic.ui.grid_recycler
+
+import android.media.Image
+
+data class GridItem(
+    val time_text: String,
+    val image: Int
+)
--- app/src/main/java/com/example/skillcampmusic/ui/grid_recycler/GridRecyclerAdapter.kt
+++ app/src/main/java/com/example/skillcampmusic/ui/grid_recycler/GridRecyclerAdapter.kt
@@ -1,23 +1,38 @@
 package com.example.skillcampmusic.ui.grid_recycler
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.FitCenter
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.example.skillcampmusic.R
 
-class GridRecyclerAdapter: RecyclerView.Adapter<GridRecyclerAdapter.GridViewHolder>() {
+class GridRecyclerAdapter(val context: Context, val list: List<GridItem>): RecyclerView.Adapter<GridRecyclerAdapter.GridViewHolder>() {
     class GridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val image: ImageView = itemView.findViewById(R.id.image)
+        val time_text: TextView = itemView.findViewById(R.id.time_text)
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GridViewHolder {
-        TODO("Not yet implemented")
+        return GridViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.grid_recycler_item, parent, false))
     }
 
     override fun onBindViewHolder(holder: GridViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        Glide
+            .with(context).load(list[position].image)
+            .transform(RoundedCorners(15))
+            .into(holder.image)
+
+        holder.time_text.text = list[position].time_text
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
     }
 }
