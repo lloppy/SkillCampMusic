1f1f91dbe1d36ba39f49364c32666d01f85df021
Полина <ankudinovapol@gmail.com>
2023-01-14 02:47:05 +0500

    added GridRecyclerFragment

--- app/src/main/java/com/example/skillcampmusic/ui/grid_recycler/GridRecyclerFragment.kt
+++ app/src/main/java/com/example/skillcampmusic/ui/grid_recycler/GridRecyclerFragment.kt
@@ -3,13 +3,25 @@ package com.example.skillcampmusic.ui.grid_recycler
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.skillcampmusic.R
 
 class GridRecyclerFragment : Fragment(R.layout.fragment_grid_recycler) {
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val list: List<GridItem> = listOf(
+            GridItem("10:00", R.drawable.imageone),
+            GridItem("21:00", R.drawable.imagetwo),
 
+            GridItem("11:00", R.drawable.imageone),
+            GridItem("22:00", R.drawable.imagetwo),
+
+            GridItem("12:00", R.drawable.imageone),
+            GridItem("23:00", R.drawable.imagetwo),
+
+            )
+        val recyclerView: RecyclerView =  view.findViewById(R.id.recyclerView)
+        recyclerView.adapter = GridRecyclerAdapter(requireContext(), list)
     }
 }
